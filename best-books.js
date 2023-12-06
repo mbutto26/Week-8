3ba9953c03994ba727153ab4b9f651b76ef2e1e5
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-12-06 10:28:19 -0800

    Update best-books.js

--- best-books.js
+++ best-books.js
@@ -1,11 +1,35 @@
 const myAPI = `bfqKpeqFxy7G14t2fA0MSuoUGfpq2Xbl`;
-const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=yourkey`;
+const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=`;
+const bookurl = `https://api.nytimes.com/svc/books/v3/lists/${year}-${month}-${date}/hardcover-fiction/best-sellers/history.json`;
 
+///lists/2019-01-20/hardcover-fiction.json
+
+//
 const formEl = document.getElementById("best-books-form");
 const yearEl = document.getElementById("year");
 const monthEl = document.getElementById("month");
 const dateEl = document.getElementById("date");
 
+const myURL = `${bookurl} ${myAPI}`;
+//
+
+fetch(myUrl)
+  .then(function (data) {
+    return data.json();
+  })
+  .then(function (responseJson) {
+    console.log(responseJson);
+
+    let bookTitle = responseJson.results.book_details[0];
+    console.log(bestBooks);
+    let bookDescrip = responseJson.results.book_details[1];
+    console.log(bookDescrip);
+    let bookAuthor = responseJson.results.book_details[3];
+    console.log(bookAuthor);
+    let bestBooks = `${bookTitle}, ${bookDescrip}, ${bookAuthor}`;
+    document.getElementById(`books-container`).innerText = bestBooks;
+  });
+//
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
