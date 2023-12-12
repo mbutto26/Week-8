2a3421b8609d3627a5749069efe8843efe98f540
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-12-12 08:28:02 -0800

    Update remove-notice.js

--- remove-notice.js
+++ remove-notice.js
@@ -1,6 +1,7 @@
-const maintenanceNotice = document.getElementById("maintenance-notice");
+const maintenanceNotice = document.getElementById("maintennce-notice");
 
 const removeNotice = function () {
   maintenanceNotice.classList.add("d-none");
 };
 setTimeout(removeNotice, 5000);
+console.log(removeNotice);
