448bc4e06e6ce1ce66ff9b011a167cbcfdde05c9
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-12-13 15:47:02 -0800

    bestbooks

--- best-books.js
+++ best-books.js
@@ -1,37 +1,37 @@
+const searchForm = document.getElementById("best-books-form");
+const searchBtn = document.getElementById("submit");
+
 const myAPI = `bfqKpeqFxy7G14t2fA0MSuoUGfpq2Xbl`;
 const url = `https://api.nytimes.com/svc/books/v3/lists/overview/hardcover-fiction.json?api-key=`;
 const bookurl = `https://api.nytimes.com/svc/books/v3/lists/overview/hardcover-fiction/best-sellers.json?api-key=`;
 
 const formEl = document.getElementById("best-books-form");
+
 const yearEl = document.getElementById("year");
 const monthEl = document.getElementById("month");
 const dateEl = document.getElementById("date");
-
 const myUrl = `${bookurl}` + `${myAPI}`;
 
 function booksEl() {
   fetch(`${myUrl}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(responseJson);
+      document.querySelector("#books-container").innerHTML =
+        data.results.lists[0].books[0].author;
+      document.querySelector("#books-container").innerHTML =
+        data.results.lists[0].books[0].title;
+      document.querySelector("#books-container").innerHTML =
+        data.results.lists[0].books[0].description;
 
-      document.getElementById("books-container") = data.book_details[0],[1], [3];
-      console.log(bestBooks);
-      
-      console.log(bookDescrip);
-      
-      console.log(bookAuthor);
-      
+      console.log(data);
     });
+  document.querySelector("#books-container").style.display = "block";
 }
-//
-formEl.addEventListener("submit", function (e) {
+
+searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const year = yearEl.value;
-  const month = monthEl.value;
-  const date = dateEl.value;
-
-  // Fetch bestselling books for date and add top 5 to page
+  booksEl(searchForm.value);
 });
 
+// Fetch bestselling books for date and add top 5 to page
