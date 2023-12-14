81160b570c3b35ba5a41c10791cabf093adc373f
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-12-14 12:12:28 -0800

    Books

--- best-books.js
+++ best-books.js
@@ -1,3 +1,7 @@
+const yearEl = document.getElementById("year");
+const monthEl = document.getElementById("month");
+const dateEl = document.getElementById("date");
+
 const searchForm = document.getElementById("best-books-form");
 const searchBtn = document.getElementById("submit");
 
@@ -5,32 +9,51 @@ const myAPI = `bfqKpeqFxy7G14t2fA0MSuoUGfpq2Xbl`;
 const url = `https://api.nytimes.com/svc/books/v3/lists/overview/hardcover-fiction.json?api-key=`;
 const bookurl = `https://api.nytimes.com/svc/books/v3/lists/overview/hardcover-fiction/best-sellers.json?api-key=`;
 
-const formEl = document.getElementById("best-books-form");
-
-const yearEl = document.getElementById("year");
-const monthEl = document.getElementById("month");
-const dateEl = document.getElementById("date");
 const myUrl = `${bookurl}` + `${myAPI}`;
 
 function booksEl() {
   fetch(`${myUrl}`)
     .then((response) => response.json())
     .then((data) => {
-      document.querySelector("#books-container").innerHTML =
-        data.results.lists[0].books[0].author;
-      document.querySelector("#books-container").innerHTML =
-        data.results.lists[0].books[0].title;
-      document.querySelector("#books-container").innerHTML =
-        data.results.lists[0].books[0].description;
+      const bookOne = data.results.lists[0].books[0];
+      const bookOneEl = [bookOne.title, bookOne.author, bookOne.description];
+      document.querySelector(".book-one").innerHTML = bookOneEl;
 
-      console.log(data);
+      const bookTwo = data.results.lists[0].books[1];
+      const bookTwoEl = [bookTwo.title, bookTwo.author, bookTwo.description];
+      document.querySelector(".book-two").innerHTML = bookTwoEl;
+
+      const bookThree = data.results.lists[0].books[2];
+      const bookThreeEl = [
+        bookThree.title,
+        bookThree.author,
+        bookThree.description,
+      ];
+      document.querySelector(".book-three").innerHTML = bookThreeEl;
+
+      const bookFour = data.results.lists[0].books[3];
+      const bookFourEl = [
+        bookFour.title,
+        bookFour.author,
+        bookFour.description,
+      ];
+      document.querySelector(".book-four").innerHTML = bookFourEl;
+
+      const bookFive = data.results.lists[0].books[4];
+      const bookFiveEl = [
+        bookFive.title,
+        bookFive.author,
+        bookFive.description,
+      ];
+      document.querySelector(".book-five").innerHTML = bookFiveEl;
+
+      console.log(booksEl);
     });
   document.querySelector("#books-container").style.display = "block";
 }
 
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
-
   booksEl(searchForm.value);
 });
 
